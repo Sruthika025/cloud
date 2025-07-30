b43768c67d8ee9d4aedb04ac75b66742ce0d9884
Sruthika025 <sruthikathammadi025@gmail.com>
2025-07-30 09:57:16 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask"
+    return "hello from flask!"
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
-
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
